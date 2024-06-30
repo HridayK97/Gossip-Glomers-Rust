1c2f8460556ca37d91c672588e4d58628cd4d7e5
Hriday Kaju <kajuh@amazon.com>
2024-06-29 17:12:00 -0700

    Implementation - fault tolerant broadcast
    Naive implementation to achieve fault tolerance with retries. A node updates it's local state, then spawns a background task to forward the broadcast request to neighbors. THe actual network call will timeout after a certain duration and retry indefinitely to mitigate network faults.

--- src/bin/broadcast.rs
+++ src/bin/broadcast.rs
@@ -1,10 +1,13 @@
 use async_trait::async_trait;
+use chrono::Utc;
 use maelstrom::protocol::Message;
 use maelstrom::{done, Node, Result, Runtime};
 use serde::{Deserialize, Serialize};
 use std::collections::{HashMap, HashSet};
 use std::sync::Arc;
+use std::time::Duration;
 use tokio::sync::RwLock;
+use tokio_context::context::Context;
 
 pub(crate) fn main() -> Result<()> {
     Runtime::init(try_main())
@@ -27,17 +30,19 @@ impl Node for Handler {
 
         match request_body {
             Ok(Request::Broadcast { message }) => {
-                let mut store = self.store.write().await;
-                if !store.contains(&message) {
-                    store.insert(message);
-
-                    for neighbor in runtime.neighbours() {
-                        runtime.call_async(neighbor, Request::Broadcast { message })
+                if !self.check_message_exists(&message).await {
+                    self.insert_message(message).await;
+                    let neighbors: Vec<String> =
+                        runtime.neighbours().map(|x| x.to_owned()).collect();
+                    for neighbor in neighbors.into_iter() {
+                        let runtime = runtime.clone();
+                        tokio::task::spawn(async move {
+                            gossip_offer_to_node(runtime.clone(), neighbor, message).await;
+                        });
                     }
                 }
                 return runtime.reply_ok(req).await;
             }
-
             Ok(Request::Read {}) => {
                 let store = self.store.read().await;
                 let data: Vec<i64> = store.clone().into_iter().collect();
@@ -51,6 +56,33 @@ impl Node for Handler {
 
             _ => done(runtime, req),
         }
+    }
+}
+
+impl Handler {
+    async fn check_message_exists(&self, message: &i64) -> bool {
+        let store = self.store.read().await;
+        store.contains(message)
+    }
+
+    async fn insert_message(&self, message: i64) {
+        let mut store = self.store.write().await;
+        store.insert(message);
+    }
+}
+
+async fn gossip_offer_to_node(runtime: Runtime, node: String, message: i64) {
+    while !runtime
+        .call(
+            // This just hangs on the await during a network partition if you don't pass a timeout!!
+            // Passing a timeout of 100 ms
+            Context::with_timeout(Duration::from_millis(100)).0,
+            node.clone(),
+            Request::Broadcast { message },
+        )
+        .await
+        .is_ok()
+    {
     }
 }
 
@@ -68,4 +100,12 @@ enum Request {
     Topology {
         topology: HashMap<String, Vec<String>>,
     },
+}
+
+fn debug(message: String) {
+    eprintln!(
+        "{:?} - {:?}",
+        message,
+        Utc::now().to_rfc3339()
+    );
 }
